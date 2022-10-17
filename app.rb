1df9596207af2eba35107a77dcdb9a4169f0e9dc
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-10-17 10:43:48 +0300

    complete

---
+++ app.rb
@@ -0,0 +1,14 @@
+puts "Hello World!"
+# puts "Hello world!"
+# puts "Hello world!"
+# puts ("I ain't got a girlfriend, I love code!")
+
+print "Pass this test, please."
+# print "Lost Haise!"
+# print "Show me love straim!"
+
+# puts [1, 2, 3].inspect
+
+p [1, 2, 3]
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
